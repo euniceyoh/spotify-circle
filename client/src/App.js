cbce37d74fc1f87b991063388ef6f432d02b8769
Carter-TE <cte12001@gmail.com>
2021-01-09 04:47:39 -0800

    Similarity v1
    Basic similarity score calculator w/ comments

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,14 @@ import React, { useState, useEffect, useCallback } from 'react'
 
 const spotifyApi = new SpotifyWebApi()
 
-let usr1 = new Object()
-let usr2 = {}
-const test_users = []
+const test_user = [] // model data base entry?? contains topSongs,topArtists
 
 function App() {
   const [tracks, setTracks] = useState([])
   const [artists, setArtists] = useState([])
   const [genres, setGenres] = useState([])
+  const [topTracks, setTopTracks] = useState([])
+  const [topArtists, setTopArtists] = useState([])
   const [trackSimScore, setTrackSimilarity] = useState([])
 
   function getHashParams() {
@@ -37,64 +37,52 @@ function App() {
       <header className="App-header">
         <a href='http://localhost:8888'>Login to Spotify</a>
         <div>
-          Track Similarity: { Object.keys(trackSimScore)}
+          Track Similarity: { (trackSimScore)}%
         </div>
         <div>
-          Songs: {Object.keys(tracks)}
+          Songs: {(tracks)}
         </div>
         <div>
-          Artists: {Object.keys(artists)}
+          Artists: {(artists)}
         </div>
         <div>
           Genres: {genres}
         </div>
+
+{//Initialize test user, copy of current user
+}        <button onClick={() => {
+          spotifyApi.getMyTopTracks({limit: 50})
+          .then((response) => {
+            let result = response.items.map(a => a.name)
+            test_user.[0] = ((Object.values(result)))
+            setTopTracks(result)
+            })
+
+            spotifyApi.getMyTopArtists({limit: 50})
+            .then((response) => {
+              let result = response.items.map(a => a.name) // naming object a and returning value of field named 'name'
+              test_user[1] = ((Object.values(result)))
+              setTopArtists(result)
+            })
+
+
+          }
+        }>init test user</button>
+
         <button onClick={() => {
-          if (test_users[0] == null) {
-            test_users[0] = usr1
+          //Can repeat this for artist, genre, etc
+          let similarity = topTracks.filter(function(val) { // filters current users topTracks with other users'
+            return test_user[0].indexOf(val) != -1
+          })
+          setTrackSimilarity((similarity.length/topTracks.length)*100)
 
-            spotifyApi.getMyTopTracks({limit: 50})
-            .then((response) => {
-              let result = response.items.map(a => a.name)
-              setTrackSimilarity(result)
-              })
-
-              spotifyApi.getMyTopArtists({limit: 50})
-              .then((response) => {
-                let result = response.items.map(a => a.name) // naming object a and returning value of field named 'name'
-                test_users.push(result)
-              })
-          }
-          /*else if (test_users[1] == null){
-
-            test_users[1] = usr2
-
-            spotifyApi.getMyTopTracks({limit: 50})
-            .then((response) => {
-              let result = response.items.map(a => a.name)
-              usr2["topSongs"] = result
-              })
-
-              spotifyApi.getMyTopArtists({limit: 50})
-              .then((response) => {
-                let result = response.items.map(a => a.name) // naming object a and returning value of field named 'name'
-                usr2["topArtists"] = result
-              })
-            }*/
-
-            let similarity = []
-            setArtists(test_users[0])
-
-
-
-          }
+        }
 
               //grab your songs
               //grab friends songs from data base
               //var arr = mysongs.filter(function sim(yoursongs) => {
               //mysongs.indexOf(yoursongs) != -1
             //}
-
-
 
 
         }>Track Similarity</button>
